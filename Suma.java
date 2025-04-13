1542df8d3843f6673b7266a535a351e82570d2b0
ElPablos <113216799+PabloAguilar22@users.noreply.github.com>
2025-04-13 08:16:38 +0200

    Update Suma.java
    Se agregó parámetros para poder introducir datos.

--- Suma.java
+++ Suma.java
@@ -1,15 +1,31 @@
+package com.mycompany.suma;
 
-package com.mycompany.saludo;
+//Importación de librerías
+import java.util.Scanner;
 
 public class Suma {
-    public static void main(String[] args){
-   
-    float a, b;
-    float suma;
-
-    suma = a + b;
+    public static void main(String [] args) {
+        
+        //Declaración de variables
+        float result;
+        float a;
+        float b;
+        
+        //Declaración del scanner para introducir datos
+        Scanner scanner = new Scanner (System.in);
     
-    System.out.printin("El resultado de la suma es: " + suma);
-
+        //Ingreso de datos
+        System.out.println("Ingresa el primer número: ");
+        a = scanner.nextFloat();
+        
+        System.out.println("Ingresa el segundo número: ");
+        b = scanner.nextFloat();
+        
+        //Operación
+        result = a + b;
+        
+        //Resultado de la operación
+        System.out.println("El resultado de la operación es: " + result);
     }
+    
 }
