f67b033650c3aecc03ff4025ec7694398daf57ca
Neri-0507 <angeldelvalle1705@gmail.com>
2025-06-25 11:36:55 -0600

    Update Sumas.java

--- Sumas.java
+++ Sumas.java
@@ -1,31 +1,22 @@
 //Importación de librerías
 import java.util.Scanner;
-
 public class Sumas {
-
     public static void main(String [] args) {
-     
         //Creación de un nuevo scanner
         Scanner scanner = new Scanner(System.in);
-        
         //declaración de variables
         int n1;
         int n2;
-        
         //Registro del primer número
         System.out.println("Ingresa el primero número: ");
         n1 = scanner.nextInt();
-        
         //Registro del segundo número
         System.out.println("Ingresa un segundo número: ");
         n2 = scanner.nextInt();
-        
         //Nuevo objeto de la clase 
         Suma suma = new Suma( n1, n2);
-        
         //Mostrar resultado
         System.out.println("El resultado de la suma es: " + suma.calcular());
-        
         //Se cierra el scanner
         scanner.close();        
     }
@@ -33,17 +24,14 @@ public class Sumas {
 
 //Clase Suma, en donde se realiza la operación correspondiente
 class Suma{
-        
     //Declaración de variables o atributos
     private int a;
     private int b;
-
     //Constructor
     public Suma(int a, int b) {
         this.a = a;
         this.b = b;
     }
-        
     //Método para realizar la suma
     public int calcular(){
         return a + b;
